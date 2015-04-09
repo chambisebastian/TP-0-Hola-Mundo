2afa25b2a4160ef04761dfafaad06bdff4ffa8d3
chambisebastian <reydeltenis_6@hotmail.com>
2015-04-09 01:24:45 -0300

    Hola Mundo TP-0
    Programa que devuelve un "Hola mundo"

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+Descrpcion: Programa que devuelve un hola mundo por consola.
+Sebastian Matias Chambi
+06/04/2015
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout <<"Hola Mundo\n";
+    system("PAUSE");
+}
